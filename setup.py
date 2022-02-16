6dbb6d1dfd46c78a6a219dbeec8535584b67951b
Ikhwan S. H <ikhwansyatricha@gmail.com>
2022-02-16 16:43:46 +0700

    Adding setup.py, MANIFEST.in, and LICENSE files

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""This module contains setup instructions for pytd."""
+import codecs
+import os
+
+from setuptools import setup
+import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+with open(os.path.join(here, "pytd", "version.py")) as fp:
+    exec(fp.read())
+
+setup(
+    name="pytd-youtube-downloader",
+    version=__version__,
+    author="Ikhwan S. H",
+    author_email="ikhwansyatricha@gmail.com",
+    packages=["pytube", "pytube.contrib"],
+    package_data={"": ["LICENSE"],},
+    url="https://github.com/perfect-less/Python-YouTube-Downloader",
+    license="The Unlicense (Unlicense)",
+    entry_points={
+        "console_scripts": [
+            "pytd = pytd.cli:main"],},
+    classifiers=[
+        "Development Status :: Beta/Unstable",
+        "Environment :: Console",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python",
+        "Topic :: Video",
+        "Topic :: Terminals",
+    ],
+    package_dir={'': 'pytd'},
+    packages=setuptools.find_packages(where='pytd'),
+    install_requires=[
+        'pytube',
+    ],
+    description=("Python application for downloading YouTube Videos."),
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    python_requires=">=3.6",
+)
